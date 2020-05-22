b06205a4daf47a91928591d78092ba78def72be8
ichibei <65130375+ichibei@users.noreply.github.com>
2020-05-22 17:54:54 +0300

    Update script1.php

--- script1.php
+++ script1.php
@@ -11,6 +11,6 @@ function sum_int($array1) {
 }
 $array1 = [1, 2, 5, '$a1'=>[1,2,3,"string1", 2.5, "string2"], 'world'=>[1,2,3,5.7,"string3",2.6,"string4"],7,9,];
 echo sum_int($array1)."\n";
-echo array_sum($array1)."\n";
-print_r ($array1);
+//echo array_sum($array1)."\n";
+//print_r ($array1);
 ?>
